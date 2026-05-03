9c88681e97573376c8fe5f64390cffe78b1b2600
moustaphasavare01 <moustaphalayethiaw@gmail.com>
2026-05-03 18:23:14 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,148 @@
+import streamlit as st
+
+st.markdown("""
+EL HADJI MAMADOU MOUSTAPHA SAVARE
+
+Technicien Supérieur en Géomatique Marine  
+
+Ancien Marin Militaire &
+
+Plongeur Professionnel
+""")
+    
+st.title("PROFIL")
+
+st.write("""
+Professionnel hautement discipliné, ancien militaire et plongeur de la
+Marine Nationale Sénégalaise formé à l’ Ecole de plongée de la Marine
+Royale Marocaine. Je suis spécialisé dans les opérations sous-marines,
+la sécurité maritime et la formation des jeunes recrues. Solide
+leadership, grande résistance au stress, et expertise reconnue en
+environnement extrême.
+""")
+
+st.header("EXPERIENCE PROFESSIONNELLE")
+st.subheader("""
+Technicien supérieur en géomatique marine
+Plongeur de bord / chef d’équipe de plongée
+""")
+st.write("""
+Marine Nationale Sénégalaise
+
+Réalisation de missions sous-marines stratégiques (inspection,
+entretien, récupération) et de sauvetage
+Opération de sécurité et de surveillance à bord de bâtiments de la
+Marine
+Maintenance et contrôle des équipements subaquatiques et de bord
+Formation en sécurité incendie, premier secours, lutte contre la
+pollution maritime.
+Encadrement de jeunes plongeurs militaires
+Gestion d'opérations de sécurité et d'intervention rapide
+Collaboration étroite avec les équipes techniques et officiers pour la
+conduite de missions complexes.
+""")
+
+st.header("CERTIFICATIONS")
+st.markdown("""
+2021-2025 
+
+Brevet de technicien Supérieur en Géomatique
+
+Certificat de plongeur de bord
+
+Certificat Pratique de plongée 
+
+Certificat de formation maritime
+
+Certificat d’embarcation 
+
+Certificat de bonne conduite
+
+""")
+
+st.header("COMPETENCES")
+st.markdown(""" 
+
+Géomatique = 
+["
+Géorefencement
+
+Base de données Géospatiales
+
+Initiation à la Télédétection
+
+Cartographie Numérique
+
+Cartographie avec maitrise des outils SIG (ArcGIS,QGIS)
+"]
+
+
+programmation =
+["
+
+Suite Microsoft Offfice
+
+Python & Languages de programmation
+
+Dessin Plan Autocad
+
+Collecte de Données avec les outils comme GPS,kobocollects
+"]
+
+
+Plongée professionnelle = 
+["
+
+Sécurité maritime et gestion de
+crise
+
+Sauvetage en mer
+
+Maintenance et logistique navale
+
+Discipline, travail d’équipe, et
+esprit d’initiative
+
+Connaissance des protocoles HSE
+offshore
+"]
+Bonne maitrise de l’informatique
+
+""")
+
+st.header("EDUCATION")
+st.markdown("""
+CENTRE DE DEVELLOPEMENT ET D'ENTREPENARIAT TECHNIQUE "LE G15"
+
+BREVET DE TECHNICIEN SUPERIEUR (BTS) 2025-2026
+
+UNIVERSITÉ DE BAMBEY
+
+licence Maths Physique
+Chimie Informatique(MPCI)
+2019 - 2021
+
+2018- 2019
+COURS PRIVES ZAC MBAO
+baccalauréat scientifique
+
+""")
+
+with st.sidebar:
+    st.title("EL HADJI MAMADOU MOUSTAPHA SAVARE")
+    st.header("Technicien Supérieur en Géomatique Marine & Chef d'équipe  Plongeur professionenl")
+
+    st.subheader("INFORMATION PERSONNELE")
+
+    st.write("""
+
+CONTACT
+
+ 📞 +221 77 717 16  01
+
+ 📧 moustaphalayethiaw@gmail.com
+
+📍 Grand Mbao , Dakar, Sénégal
+
+""")
+
